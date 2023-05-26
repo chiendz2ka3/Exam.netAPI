2fd0bad264faa7875bfb2e819296fc0897018072
ChienTQ2003 <anhchienbs.1@gmail.com>
2023-05-26 18:46:28 +0700

    first commit

---
+++ Controllers/ExamController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExamMVC.Controllers
+{
+    public class ExamController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
